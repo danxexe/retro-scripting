c3ba8285dfefb238b1ebee133713330edc49d51e
danxexe <danx.exe@gmail.com>
2024-12-14 20:07:30 -0300

    Display evolution requirements

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import { JSONRPCClient } from './json-rpc-2.0.js'
-import { digimon_hex_to_name, stats } from './digimon-world.js'
+import { stats, digimonNames, evolutionPaths, evolutionRequirements } from './digimon-world.js'
+import { reactive, html } from './arrow.js'
 
 const client = new JSONRPCClient((jsonRPCRequest) =>
   fetch("http://localhost:3030", {
@@ -23,16 +24,62 @@ const stats_table = document.querySelector("#stats-table");
 const stats_row_template = document.querySelector("#stats-row-template");
 let old_data = {}
 
-async function update () {
+let state = reactive({
+  partner: {},
+  evoPaths: [],
+});
+
+window.state = state;
+
+function format_value(value) {
+  if (Array.isArray(value)) {
+    let [min, max] = value;
+    min = min == Infinity ? "∞" : min;
+    max = max == Infinity ? "∞" : max;
+    return html`${min} <span style="font-size: 0.8em">≷</span> ${max}`;
+  }
+
+  return value;
+}
+
+const digimon_template = (name, stats, props) => {
+  return html`
+  <table>
+    <thead>
+      <tr>
+        <th colspan="2">${() => name}</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${() => Object.entries(props).map(([key, display]) => html`
+        <tr class="flash-once">
+          <th>${display}</th>
+          <td>${format_value(stats[key])}</td>
+        </tr>
+      `)}
+    </tbody>
+  </table>`
+}
+
+html`
+  ${() => digimon_template(state.partner.name, state.partner, stats)}
+`(document.querySelector("#partner"))
+
+html`
+  ${() => state.evoPaths.map(([name, requirements]) => digimon_template(name, requirements, stats))}
+`(document.querySelector("#evolution-paths"))
+
+async function update() {
   const response = await client.request("read_memory", {
     u8: {
       happiness: 0x13848A,
       discipline: 0x138488,
       mistakes: 0x13847e,
       weight: 0x1384a2,
+      battles: 0x1384b4
     },
     u16le: {
-      digimon: 0x1557b0,
+      digimon_id: 0x1557b0,
       off: 0x1557e0,
       def: 0x1557e2,
       speed: 0x1557e4,
@@ -41,33 +88,15 @@ async function update () {
       mp: 0x1557f2,
     }
   });
+  
+  state.partner = reactive({ ...response["u8"], ...response["u16le"] });
+  state.partner.name = digimonNames.get(state.partner.digimon_id);
 
-  const data = {...response["u8"], ...response["u16le"]}
-  const digimon_name = digimon_hex_to_name.get(data["digimon"]);
+  state.evoPaths = evolutionPaths[state.partner.name]
+    .map((digimon) => [digimon, evolutionRequirements[digimon]])
+    .filter(([_, req]) => req.special == undefined);
 
-  document.querySelector("#digimon-name").innerText = digimon_name;
-
-  for (const prop in stats) {
-    const value = data[prop];
-    const old_value = old_data[prop];
-
-    if (value != old_value) {
-      const clone = stats_row_template.content.cloneNode(true);
-
-      clone.querySelector("tr").classList.add(prop, "flash-once");
-      clone.querySelector("th").innerText = stats[prop];
-      clone.querySelector("td").innerText = data[prop];
-      
-      const old_element = stats_table.querySelector("tr." + prop);
-      if (old_element) {
-        stats_table.replaceChild(clone, old_element);
-      } else {
-        stats_table.appendChild(clone);
-      }
-    }  
-  }
-
-  old_data = data;
+  old_data = state.partner;
 }
 
 function sleep(ms) {
@@ -84,7 +113,7 @@ async function loop() {
       run = false
       console.log(e)
     }
-    
+
     await sleep(1000);
   }
 }
